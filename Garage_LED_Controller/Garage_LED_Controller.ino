f533aebed5b4ae59c7d3a7c89d17c742d32037c3
Siddharth10203 <43057383+Siddharth10203@users.noreply.github.com>
2020-03-14 14:46:27 -0700

    Add files via upload

---
+++ Garage_LED_Controller/Garage_LED_Controller.ino
@@ -0,0 +1,17 @@
+int analogPin = 0;
+float val = 0;
+double voltage;
+int lumens;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(analogPin);
+  voltage = val*5/1023;
+  Serial.println(voltage);
+  delay(500);
+}
